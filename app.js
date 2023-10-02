0d8bdd51bb40f65d2b570fe9941f448af1c70a83
ulisesGutierrez <gu362937@uaeh.edu.mx>
2023-10-01 21:20:36 -0600

    info

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 /*
 Bootstrap:
 Framework CSS para desarrollar sitios web responsive (adaptables) y mobile-first.
+
+Framework:
+Estructura (base) que puedes usar para desarrollar tu aplicación o sitio web de
+forma más rápida y eficiente.
+
+Framework CSS:
+Clases que nos permiten personalizar el estilo de los elementos HTML de nuestro
+sitio web.
+
+Adaptable:
+Que se adapta a distintos dispositivos en base a su tamaño y orientación.
+(en desarrollo web)
+
+Grid de Bootstrap:
+Conjunto de contenedores, filas y columnas que definen como de va a presentar y
+a alinear el contenido.
+
 */
